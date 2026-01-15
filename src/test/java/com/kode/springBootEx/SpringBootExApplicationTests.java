746e20bafb9c96078e40467d380ec08f2cd5aaaa
vivek kodepaka <kodepakavivek97@gmail.com>
2026-01-15 15:23:56 +0530

    first commit

---
+++ src/test/java/com/kode/springBootEx/SpringBootExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kode.springBootEx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
